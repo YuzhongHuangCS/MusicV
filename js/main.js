d0dc1ecf7e7c0cd159a57598369c23925fbce5d9
Yuzo <hyzgog@gmail.com>
2015-04-25 21:26:09 +0800

    motion detect draft

--- js/main.js
+++ js/main.js
@@ -66,6 +66,53 @@
 		
 		a.loadSound();
 
+
+		var canvas = document.getElementById("canvas-blended");
+
+		var ctx = canvas.getContext("2d");
+		ctx.fillStyle = "#FF0000";
+		ctx.strokeStyle = "#00FF00";
+		ctx.lineWidth = 5;
+
+		console.log("Inintializing");
+		var camMotion = CamMotion.Engine();
+		console.log(camMotion);
+		camMotion.on("error", function (e) {
+			console.log("error", e);
+		});
+		console.log(camMotion);
+		camMotion.on("streamInit", function(e) {
+			console.log("webcam stream initialized", e);
+		});
+		camMotion.onMotion(CamMotion.Detectors.LeftMotion, function () {
+			console.log("AAAAAAAAAAAAAAAH Left motion detected");
+		});
+		camMotion.onMotion(CamMotion.Detectors.RightMotion, function () {
+			console.log("AAAAAAAAAAAAAAAH Right motion detected");
+		});
+		camMotion.onMotion(CamMotion.Detectors.DownMotion, function () {
+			console.log("AAAAAAAAAAAAAAAH Down motion detected");
+		});
+		camMotion.onMotion(CamMotion.Detectors.UpMotion, function () {
+			console.log("AAAAAAAAAAAAAAAH Up motion detected");
+		});
+		camMotion.on("frame", function () {
+			ctx.clearRect ( 0 , 0 , 640, 480);
+			var point = camMotion.getMovementPoint(true);
+			console.log(point);
+			// draw a circle
+			ctx.beginPath();
+			ctx.arc(point.x, point.y, point.r, 0, Math.PI*2, true);
+			ctx.closePath();
+			if (camMotion.getAverageMovement(point.x-point.r/2, point.y-point.r/2, point.r, point.r)>4) {
+				ctx.fill();
+				var str = 'rotateX(' + point.x/10 + 'deg)' + 'rotateY(' + point.y/10 + 'deg)';
+				$('svg').css('transform', str);
+			} else {
+				ctx.stroke();
+			}
+		});
+		camMotion.start();
 	};
 
 	a.bind = function() {
